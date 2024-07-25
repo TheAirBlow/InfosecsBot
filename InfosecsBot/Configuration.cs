b6d5f481e5e440eed06cd3567240b8357cd00c5f
TheAirBlow <theairblow@gmail.com>
2024-07-25 13:37:35 +0300

    another bugfix

--- InfosecsBot/Configuration.cs
+++ InfosecsBot/Configuration.cs
@@ -51,7 +51,7 @@ public class Configuration {
     /// <summary>
     /// Next obed check
     /// </summary>
-    public DateTime NextCheck { get; set; } = ObedManager.CurrentTime;
+    public DateTime NextCheck { get; set; } = DateTime.Now;
     
     /// <summary>
     /// Telegram bot token
